a09941070c806dd7ba5056642d8e03776689f62d
samatbek01 <samatabjaparov05@gmail.com>
2024-02-13 19:29:09 +0600

    first commit

--- src/component/List.js
+++ src/component/List.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const List = (props) => {
     return (
-        <div>
+        <div >
             {
                 props.newUser.map((item) =>
-                <div>
+                <div style={{ marginTop: '20px' ,border: '3px orange solid', display: 'flex', alignItems: 'center', gap: '10px', paddingLeft: '30px'}}>
                     <h2>{item.title}</h2>
                     <p>{item.id}</p>
                     <span>{item.completed}</span>
